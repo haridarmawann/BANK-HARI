0b3db83c65af12b27a99660c8418c2bcab1af1c5
hari <hdarmawan472@gmail.com>
2026-07-06 10:22:15 +0700

    fix: revision #2 - add accountNumber and balance

--- src/main/java/bank_bank/demo/service/NasabahService.java
+++ src/main/java/bank_bank/demo/service/NasabahService.java
@@ -14,7 +14,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -98,9 +97,5 @@ public class NasabahService {
                 page,
                 result.getTotalElements()
         );
-    }
-
-    public void isiSaldo(String accountNumber, BigDecimal saldo){
-
     }
 }
